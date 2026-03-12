56aa42196f74e1226d5c76aa2fcd1961076fe8cb
scoonie <48395518+scoonie@users.noreply.github.com>
2026-03-12 17:11:51 +0000

    Update mantine.style.ts

--- frontend/src/styles/mantine.style.ts
+++ frontend/src/styles/mantine.style.ts
@@ -1,21 +1,24 @@
 import { MantineThemeOverride } from "@mantine/core";
 
+const maroonShades = [
+  "#F4E2E4", // [0]
+  "#E7C2C7", // [1]
+  "#D89DA6", // [2]
+  "#C67683", // [3]
+  "#B24E5E", // [4]
+  "#9E2A3B", // [5]
+  "#8A2433", // [6]
+  "#751F2A", // [7]
+  "#5E1A23", // [8]
+  "#4A151C", // [9]
+];
+
 export default <MantineThemeOverride>{
   colors: {
-    maroon: [
-      "#F4E2E4", // [0] Lightest (subtle backgrounds)
-      "#E7C2C7", // [1]
-      "#D89DA6", // [2]
-      "#C67683", // [3]
-      "#B24E5E", // [4]
-      "#9E2A3B", // [5] Primary Button Color (Rich Maroon)
-      "#8A2433", // [6] Hover State
-      "#751F2A", // [7]
-      "#5E1A23", // [8]
-      "#4A151C", // [9] Darkest
-    ],
+    maroon: maroonShades,
+    victoria: maroonShades,
   },
-  primaryColor: "maroon", // Tell the app to use the maroon array
+  primaryColor: "maroon",
   components: {
     Modal: {
       styles: (theme) => ({
